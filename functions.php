b631a8aca9000f5bf0ffc3eeb65ac7f24c5d6e17
Luana Carvalho <luana2021ubuntu@gmail.com>
2021-01-04 13:01:56 -0300

    Criaçao de Classe para personalizaçao de menu detalhada

--- functions.php
+++ functions.php
@@ -5,6 +5,12 @@ define( 'MOON_DEV_MODE', true );
 
 // Includes
 include( get_theme_file_path( '/includes/front/enqueue.php' ) );
+include( get_theme_file_path( '/includes/setup.php'));
+include( get_theme_file_path( '/includes/custom-nav-walker.php'));
+
 
 //Hooks
 add_action( 'wp_enqueue_scripts', 'moon_enqueue' );
+add_action( 'after_setup_theme', 'moon_setup_theme' );
+
+// Shortcodes
--- index.php
+++ index.php
@@ -13,8 +13,27 @@
     <title>Moon Blog</title>
 </head>
 <body>
-    
+    <!-- Primary Navigation
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <nav id="primary-menu" class="style-2">
+        <div class="container clearfix">
+            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
 
+                <!-- Main Menu 
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                <?php
+                if(has_nav_menu( 'primary' )){
+                    wp_nav_menu([
+                        'theme_location'    => 'primary',
+                        'container'         => false,
+                        'fallback_cb'       => false,
+                        'depth'             => 4,
+                        'walker'            => new MOON_Custom_Nav_Walker
+                    ]);
+                }
+                ?>
+        </div>
+    </nav>
 
     <?php wp_footer(); ?>
 </body>
