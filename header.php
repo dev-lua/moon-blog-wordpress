c317ef3d61188b1f59b5b89f192b043d57186e6a
Luana Carvalho <luana2021ubuntu@gmail.com>
2021-01-04 13:33:04 -0300

    Adicionei a funcçao header e footer e organizei essas duas partes em arquivos separados para questao de organizaçao

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <!-- Stylesheets 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <?php wp_head(); ?>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Document Title 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <title>Moon Blog</title>
+</head>
+<body <?php body_class( 'stretched no-transition' ) ; ?>>
+    <!-- Primary Navigation
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <nav id="primary-menu" class="style-2">
+        <div class="container clearfix">
+            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+                <!-- Main Menu 
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                <?php
+                if(has_nav_menu( 'primary' )){
+                    wp_nav_menu([
+                        'theme_location'    => 'primary',
+                        'container'         => false,
+                        'fallback_cb'       => false,
+                        'depth'             => 4,
+                        'walker'            => new MOON_Custom_Nav_Walker
+                    ]);
+                }
+                ?>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,40 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <!-- Stylesheets 
+<!-- header-v2.php >>> get_headder( 'v2' ); -->
+<?php get_header(); ?>
+    <!-- Content
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <?php wp_head(); ?>
+    <section id="content">
+    
+    </section>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Document Title 
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <title>Moon Blog</title>
-</head>
-<body>
-    <!-- Primary Navigation
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <nav id="primary-menu" class="style-2">
-        <div class="container clearfix">
-            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
-
-                <!-- Main Menu 
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-                <?php
-                if(has_nav_menu( 'primary' )){
-                    wp_nav_menu([
-                        'theme_location'    => 'primary',
-                        'container'         => false,
-                        'fallback_cb'       => false,
-                        'depth'             => 4,
-                        'walker'            => new MOON_Custom_Nav_Walker
-                    ]);
-                }
-                ?>
-        </div>
-    </nav>
-
-    <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
